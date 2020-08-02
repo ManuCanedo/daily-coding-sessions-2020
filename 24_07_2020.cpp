4d0991b51accb02a0c6c15ddc44b6675e7b03f19
Manu Canedo <chapi.gal@gmail.com>
2020-08-02 10:28:24 +0100

    Update 24_07_2020.cpp

--- 24_07_2020.cpp
+++ 24_07_2020.cpp
@@ -1,3 +1,5 @@
+////////// CHALLENGE DESCRIPTION
+
 // A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
 
 // For example, array A such that:
@@ -38,73 +40,74 @@
 // N is an integer within the range [2..100,000];
 // each element of array A is an integer within the range [−10,000..10,000].
 
+////////// SOLUTION
 // FIRST VERSION
 
-#include <vector>
+// #include <vector>
 
-std::vector<int> getNextAvgVectorR (std::vector<int>& A, std::vector<int>& avgVector, int newAvgLength);
+// std::vector<int> getNextAvgVectorR (std::vector<int>& A, std::vector<int>& avgVector, int newAvgLength);
 
-float lowestAvg;
-unsigned int lowestAvgIndex;
+// float lowestAvg;
+// unsigned int lowestAvgIndex;
 
-int solution(std::vector<int> &A) {
-    std::vector<int> lowAvgOfTwo;
+// int solution(std::vector<int> &A) {
+//     std::vector<int> lowAvgOfTwo;
 
-    lowestAvg = A[0];
-    lowestAvgIndex = 0;
-    int previousInt = A[0];
+//     lowestAvg = A[0];
+//     lowestAvgIndex = 0;
+//     int previousInt = A[0];
     
-    for (unsigned int i = 1; i < A.size()-1; ++i)
-    {
-        if ((previousInt + A[i])/2 < 2*lowestAvg)
-        {
-            lowAvgOfTwo.push_back(i-1);
+//     for (unsigned int i = 1; i < A.size()-1; ++i)
+//     {
+//         if ((previousInt + A[i])/2 < 2*lowestAvg)
+//         {
+//             lowAvgOfTwo.push_back(i-1);
             
-            if ((previousInt + A[i])/2 < lowestAvg)
-            {
-                lowestAvg = (previousInt + A[i])/2;
-                lowestAvgIndex = i-1;    
-            }
-        }
-        previousInt = A[i];
-    }
+//             if ((previousInt + A[i])/2 < lowestAvg)
+//             {
+//                 lowestAvg = (previousInt + A[i])/2;
+//                 lowestAvgIndex = i-1;    
+//             }
+//         }
+//         previousInt = A[i];
+//     }
     
-    std::vector<int> nextLowAvg = lowAvgOfTwo;
-    for (unsigned int i = 3; i < A.size(); ++i)
-    {
-        nextLowAvg = getNextAvgVectorR (A, nextLowAvg, i);
-        if (nextLowAvg.empty())
-        {
-            break;
-        }
-    }
+//     std::vector<int> nextLowAvg = lowAvgOfTwo;
+//     for (unsigned int i = 3; i < A.size(); ++i)
+//     {
+//         nextLowAvg = getNextAvgVectorR (A, nextLowAvg, i);
+//         if (nextLowAvg.empty())
+//         {
+//             break;
+//         }
+//     }
     
-    return lowestAvgIndex;
-}
+//     return lowestAvgIndex;
+// }
 
-inline std::vector<int> getNextAvgVectorR (std::vector<int>& A, std::vector<int>& avgVector, int newAvgLength)
-{
-    std::vector<int> nextAvgVector;
-    float elementSum = 0;
+// inline std::vector<int> getNextAvgVectorR (std::vector<int>& A, std::vector<int>& avgVector, int newAvgLength)
+// {
+//     std::vector<int> nextAvgVector;
+//     float elementSum = 0;
     
-    for (unsigned int i = 0; i < avgVector.size(); i++)
-    {
-        for (int j = 0; j < newAvgLength; ++j)
-        {
-           elementSum += A[avgVector[i] + j]; 
-        }
-        if (elementSum / newAvgLength < 2*lowestAvg)
-        {
-            nextAvgVector.push_back(avgVector[i]);
-            if (elementSum / newAvgLength < lowestAvg)
-            {
-                lowestAvg = elementSum / newAvgLength;
-                lowestAvgIndex = avgVector[i];    
-            }
-        }
-    }
+//     for (unsigned int i = 0; i < avgVector.size(); i++)
+//     {
+//         for (int j = 0; j < newAvgLength; ++j)
+//         {
+//            elementSum += A[avgVector[i] + j]; 
+//         }
+//         if (elementSum / newAvgLength < 2*lowestAvg)
+//         {
+//             nextAvgVector.push_back(avgVector[i]);
+//             if (elementSum / newAvgLength < lowestAvg)
+//             {
+//                 lowestAvg = elementSum / newAvgLength;
+//                 lowestAvgIndex = avgVector[i];    
+//             }
+//         }
+//     }
 
-    return nextAvgVector;
-}
+//     return nextAvgVector;
+// }
 
 // WILL CONTINUE
