8a1421ff7b4cb7eef48be9c55253fe467fc75557
Manu Canedo <chapi.gal@gmail.com>
2020-08-02 12:39:21 +0100

    Update 22_07_2020.cpp

--- 22_07_2020.cpp
+++ 22_07_2020.cpp
@@ -16,6 +16,7 @@
 // K is an integer within the range [1..2,000,000,000];
 // A ≤ B.
 
+
 ////////// SOLUTION
 
 int solution(int A, int B, int K) 
@@ -42,6 +43,7 @@ int solution(int A, int B, int K)
 ////////// MAX ~ 0(1)
 ////////// AVG ~ 0(1)
 
+
 ////////// CHALLENGE DESCRIPTION
 
 // A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
@@ -59,6 +61,7 @@ int solution(int A, int B, int K)
 //   A[2] = 0
 //   A[3] = 1
 //   A[4] = 1
+
 // We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
 
 // Write a function:
@@ -76,12 +79,14 @@ int solution(int A, int B, int K)
 //   A[2] = 0
 //   A[3] = 1
 //   A[4] = 1
+
 // the function should return 5, as explained above.
 
 // Write an efficient algorithm for the following assumptions:
 
 // N is an integer within the range [1..100,000];
 // each element of array A is an integer that can have one of the following values: 0, 1.
+
 
 ////////// SOLUTION
 
