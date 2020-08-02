6dab1aaf92f8765271497444d0f304f31202a24f
Manu Canedo <chapi.gal@gmail.com>
2020-08-02 10:25:34 +0100

    Update 23_07_2020.cpp

--- 23_07_2020.cpp
+++ 23_07_2020.cpp
@@ -1,3 +1,5 @@
+////////// CHALLENGE DESCRIPTION
+
 // A DNA sequence can be represented as a string consisting of the letters A, C, G and T, which correspond to the types of successive nucleotides in the sequence. Each nucleotide has an impact factor, which is an integer. Nucleotides of types A, C, G and T have impact factors of 1, 2, 3 and 4, respectively. You are going to answer several queries of the form: What is the minimal impact factor of nucleotides contained in a particular part of the given DNA sequence?
 
 // The DNA sequence is given as a non-empty string S = S[0]S[1]...S[N-1] consisting of N characters. There are M queries, which are given in non-empty arrays P and Q, each consisting of M integers. The K-th query (0 ≤ K < M) requires you to find the minimal impact factor of nucleotides contained in the DNA sequence between positions P[K] and Q[K] (inclusive).
@@ -35,8 +37,8 @@
 // P[K] ≤ Q[K], where 0 ≤ K < M;
 // string S consists only of upper-case English letters A, C, G, T.
 
-
-// CODE THAT FAILED THE PERFORMANCE TEST 1
+////////// SOLUTION
+// CORRECT BUT INEFFICIENT CODE 2
 
 // #include <vector>
 // #include <string>
@@ -70,7 +72,7 @@
 //     return result;
 // }
 
-// // CODE THAT FAILED THE PERFORMANCE TEST 2
+// // CORRECT BUT INEFFICIENT CODE 2
 
 // std::vector<int> solution(std::string &S, std::vector<int> &P, std::vector<int> &Q) {
     
@@ -209,7 +211,7 @@
 //     return result;
 // }
 
-// CODE THAT PASSED THE PERFORMANCE TEST
+////////// SOLUTION
 
 std::vector<int> solution(std::string &S, std::vector<int> &P, std::vector<int> &Q) {
     
@@ -321,3 +323,8 @@ std::vector<int> solution(std::string &S, std::vector<int> &P, std::vector<int> 
     }
     return result;
 }
+
+////////// CORRECT BEHAVIOUR
+////////// TIME COMPLEXITY:
+////////// MAX ~ 0(N)
+////////// AVG ~ 0(N)
