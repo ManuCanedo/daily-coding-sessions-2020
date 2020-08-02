47478bac6082dd36468a46cdaa14db2488e18d55
Manu Canedo <chapi.gal@gmail.com>
2020-08-02 10:28:12 +0100

    Update 25_07_2020.cpp

--- 25_07_2020.cpp
+++ 25_07_2020.cpp
@@ -1,4 +1,4 @@
-// CONTINUATION 24_07
+////////// CHALLENGE DESCRIPTION
 
 // A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
 
@@ -39,6 +39,8 @@
 
 // N is an integer within the range [2..100,000];
 // each element of array A is an integer within the range [−10,000..10,000].
+
+// FIRST VERSION
 
 // #include <vector>
 // #include <map>
@@ -81,42 +83,42 @@
 //     return lowestAvgIndex;
 // }
 
-/////////////////// SECOND VERSION
+// /////////////////// SECOND VERSION
 
-#include <vector>
+// #include <vector>
 
-int solution(std::vector<int> &A) {
+// int solution(std::vector<int> &A) {
     
-    std::map<float,unsigned int> slices;
-    float currentAvg;
-    int currentSum;
-    unsigned int currentIndex, currentLength;
-    unsigned int lowestAvgIndex;
+//     std::map<float,unsigned int> slices;
+//     float currentAvg;
+//     int currentSum;
+//     unsigned int currentIndex, currentLength;
+//     unsigned int lowestAvgIndex;
 
     
     
-    for (unsigned int i = 0; i < A.size(); )
-    {
-        currentIndex = i, currentLength = 2;
-        currentSum = A[i] + A[i+1];
-        currentAvg = currentSum / currentLength;
-        bool bSearchR = true, bSearchL = true;
+//     for (unsigned int i = 0; i < A.size(); )
+//     {
+//         currentIndex = i, currentLength = 2;
+//         currentSum = A[i] + A[i+1];
+//         currentAvg = currentSum / currentLength;
+//         bool bSearchR = true, bSearchL = true;
         
-        while (bSearchR || bSearchL)
-        {
-            if ((currentSum + A[currentLength]) / (currentLength+1) < currentAvg)
-            {
-                currentAvg = (currentSum + A[currentLength]) / (currentLength+1);
-                currentLength++;
-            }
-            if (currentIndex > 0 && (currentSum + A[currentLength]) / (currentLength+1) < currentAvg)
-            {
+//         while (bSearchR || bSearchL)
+//         {
+//             if ((currentSum + A[currentLength]) / (currentLength+1) < currentAvg)
+//             {
+//                 currentAvg = (currentSum + A[currentLength]) / (currentLength+1);
+//                 currentLength++;
+//             }
+//             if (currentIndex > 0 && (currentSum + A[currentLength]) / (currentLength+1) < currentAvg)
+//             {
                 
-            }
-        }
-    }
+//             }
+//         }
+//     }
     
-    return lowestAvgIndex;
-}
+//     return lowestAvgIndex;
+// }
 
 // WILL CONTINUE
