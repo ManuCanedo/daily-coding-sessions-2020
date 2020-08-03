5fdf6936893b58de77b847263a96881f1295913c
Manu Canedo <chapi.gal@gmail.com>
2020-08-03 20:40:22 +0100

    Update 28_07_2020.cpp

--- 28_07_2020.cpp
+++ 28_07_2020.cpp
@@ -10,6 +10,7 @@
 
 //  A[0] = 2    A[1] = 1    A[2] = 1
 //  A[3] = 2    A[4] = 3    A[5] = 1
+
 // the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
 
 // Write an efficient algorithm for the following assumptions:
