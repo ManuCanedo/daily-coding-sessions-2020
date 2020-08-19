e1e7c3f3c752d0fec8219419111a9cfa2b0a854a
Manu Canedo <chapi.gal@gmail.com>
2020-08-19 19:07:46 +0200

    Add files via upload

---
+++ DataStructures/19_08_2020_vector.cpp
@@ -0,0 +1,75 @@
+////////// DATA STRUCTURES PRACTICE
+
+// Implementing my own version of the std::vector data structure.
+// In this file I define struct Vector3 and the corresponding templated functions to print Vectors and Vector3s.
+
+#include <iostream>
+#include "19_08_2020_Vector.h"
+
+struct Vector3
+{
+    float x = 0.0f, y = 0.0f, z = 0.0f;
+
+    Vector3() {}
+    Vector3(float scalar)
+        : x(scalar), y(scalar), z(scalar) {}
+    Vector3(float x, float y, float z)
+        : x(x), y(y), z(z) {}
+    
+    Vector3(const Vector3& other)
+        : x(other.x), y(other.y), z(other.z)
+    {
+        // std::cout << "Copied\n";
+    }
+    ~Vector3()
+    {
+        // std::cout << "Destroyed\n";
+    }
+
+    Vector3& operator=(const Vector3& other)
+    {
+        // std::cout << "Copied\n";
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+    }
+    Vector3& operator=(const Vector3&& other)
+    {
+        // std::cout << "Move\n";
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+    }
+};
+
+template<typename T>
+void PrintVector(const Vector<T>& vector)
+{
+    for (size_t i = 0; i < vector.Size(); ++i)
+        std::cout << vector[i] << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+}
+
+template<>
+void PrintVector(const Vector<Vector3>& vector)
+{
+    for (size_t i = 0; i < vector.Size(); ++i)
+        std::cout << vector[i].x << ", " << vector[i].y << ", " << vector[i].z << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+}
+
+int main()
+{
+    Vector<Vector3> vector;
+    vector.PushBack(Vector3(1.0f));
+    vector.PushBack(Vector3());
+    vector.PushBack(Vector3(0.2f, 0.4f, 0.8f));
+
+    PrintVector(vector);
+    std::cin.get();
+}
+
+
+
---
+++ DataStructures/19_08_2020_vector.h
@@ -0,0 +1,49 @@
+////////// DATA STRUCTURES PRACTICE
+
+// Implementing my own version of the std::vector data structure.
+// In this file I define the Vector class.
+
+template<typename T>
+class Vector 
+{
+public:
+    Vector()
+    {
+        // Allocate initial memory
+        ReAlloc(2);
+    }
+
+    T PushBack(const T& value)
+    {
+        if (m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity/2);
+
+        m_Data[m_Size++] = value;
+        return value;
+    }
+
+    const T& operator[](size_t index) const { return m_Data[index]; }
+    T& operator[](size_t index) { return m_Data[index]; }
+    size_t Size() const { return m_Size; }
+
+private:
+    void ReAlloc(size_t newCapacity)
+    {
+        T* newBlock = new T[newCapacity];
+
+        if (newCapacity < m_Size)
+            m_Size = newCapacity;
+            
+        for (size_t i = 0; i < m_Size; ++i)
+            newBlock[i] = m_Data[i];
+
+        delete[] m_Data;
+        m_Data = newBlock;
+        m_Capacity = newCapacity;
+    }
+
+private:
+    T* m_Data = nullptr;
+    size_t m_Size = 0;
+    size_t m_Capacity = 0;
+};
