0a6bf681ea84770ab36ea963bf438f754fbf019a
Manu Canedo <chapi.gal@gmail.com>
2020-08-19 13:46:06 +0200

    Add files via upload

--- 18_08_2020_algorithm.cpp
+++ 18_08_2020_algorithm.cpp
@@ -30,6 +30,7 @@
 ////////// SOLUTION
 // Attempted with prime factorization (algorithm based in Sieve of Erathostenes). It works only with relatively small numbers.
 // Current algorithm time complexity is O(N+M). Tomorrow will rework it to use Euclidean Algorithm.
+// Solved 19_08_2020 with time complexity O(log(N+M)).
 
 #include <vector>
 #include <algorithm>
@@ -39,7 +40,7 @@ int solution(int N, int M)
     int sieveSize = (N>M) ? N : M;
     int sieveIndexN = N, sieveIndexM = M, MCD = 1;
     std::vector<int> sieve(sieveSize+1,0);
-    std::vector<int> primeFactorsN, primeFactorsM, MCDFactors;
+    std::vector<int> primeFactorsN, primeFactorsM;
 
     for (int i = 2; i*i <= sieveSize; ++i)
         if (sieve[i] == 0)
@@ -63,6 +64,7 @@ int solution(int N, int M)
     std::sort(primeFactorsN.begin(), primeFactorsN.end());
     std::sort(primeFactorsM.begin(), primeFactorsM.end());
 
+    std::vector<int> MCDFactors;
     for (size_t i = 0, j = 0; i < primeFactorsN.size() && j < primeFactorsM.size(); ++i)
     {
         while (primeFactorsM[j] < primeFactorsN[i])
