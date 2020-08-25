46f3b956e527b5a7304ee42f8ef36f82deb636dd
Manu Canedo <chapi.gal@gmail.com>
2020-08-25 18:32:05 +0100

    Update 08_14_Peaks.cpp

--- Algorithms_Prime&CompositeNumbers/08_14_Peaks.cpp
+++ Algorithms_Prime&CompositeNumbers/08_14_Peaks.cpp
@@ -86,29 +86,19 @@ int solution(std::vector<int> &A)
         return 0;
 
     for (auto it = (A.begin() + 1); it != (A.end() - 1); ++it)
-    {
         if (*(it - 1) < *it && *it > *(it + 1))
             peaks.push_back(it - A.begin());
-    }
 
     if (peaks.empty())
         return 0;
 
     for (long int i = 1; i * i <= elements; i++)
-    {
         if (elements % i == 0)
-        {
             blocks.push_back(i);
-        }
-    }
-
     for (int i = blocks.size() - 1; i >= 0; --i)
-    {
         blocks.push_back(elements / blocks[i]);
-    }
 
     std::reverse(blocks.begin(), blocks.end());
-    
     for (auto it = blocks.begin(); it != blocks.end(); ++it)
     {
         lowLimit = 0;
@@ -121,9 +111,7 @@ int solution(std::vector<int> &A)
                 topLimit += *it;
             }
             if (lowLimit == elements)
-            {
                 maxBlocks = elements / (*it);
-            }
         }
     }
 
