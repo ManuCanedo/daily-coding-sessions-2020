16cced36a862414829bef245a987759e31dd063d
Manu Canedo <chapi.gal@gmail.com>
2020-08-25 18:18:00 +0100

    Update 08_08_MaxProfit.cpp

--- Algorithms_MaxSliceProblem/08_08_MaxProfit.cpp
+++ Algorithms_MaxSliceProblem/08_08_MaxProfit.cpp
@@ -10,6 +10,7 @@
 //   A[3] = 21366
 //   A[4] = 21013
 //   A[5] = 21367
+
 // If a share was bought on day 0 and sold on day 2, a loss of 2048 would occur because A[2] − A[0] = 21123 − 23171 = −2048. If a share was bought on day 4 and sold on day 5, a profit of 354 would occur because A[5] − A[4] = 21367 − 21013 = 354. Maximum possible profit was 356. It would occur if a share was bought on day 1 and sold on day 5.
 
 // Write a function,
@@ -26,6 +27,7 @@
 //   A[3] = 21366
 //   A[4] = 21013
 //   A[5] = 21367
+
 // the function should return 356, as explained above.
 
 // Write an efficient algorithm for the following assumptions:
