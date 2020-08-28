21fcdc819abca96720438f168aba8633cef4cac8
Manu Canedo <chapi.gal@gmail.com>
2020-08-28 13:57:33 +0100

    Update and rename Algorithms_PrefixSums/07_22_CountDiv.cpp to Algorithms_PrefixSums/CountDiv/07_22_solution.cpp

---
+++ Algorithms_PrefixSums/CountDiv/07_22_solution.cpp
@@ -0,0 +1,25 @@
+////////// SOLUTION
+
+int solution(int A, int B, int K) 
+{
+    int factors = 0, rangeStart = A;
+    
+    if (A == 0)
+        factors++;
+        
+    if (K > B)
+        return factors;
+
+    rangeStart = (A > K) ? A: K;
+    
+    if (rangeStart%K == 0)
+        factors++;
+        
+    factors += (B-rangeStart)/K;
+    return factors;
+}
+
+////////// CORRECT BEHAVIOUR
+////////// TIME COMPLEXITY:
+////////// MAX ~ 0(1)
+////////// AVG ~ 0(1)
