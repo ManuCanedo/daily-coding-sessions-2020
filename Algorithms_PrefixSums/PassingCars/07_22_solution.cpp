ea70d014fba748b6cef5962a12b7a13e21f211a9
Manu Canedo <chapi.gal@gmail.com>
2020-08-28 13:59:41 +0100

    Update and rename Algorithms_PrefixSums/07_22_PassingCars.cpp to Algorithms_PrefixSums/PassingCars/07_22_solution.cpp

---
+++ Algorithms_PrefixSums/PassingCars/07_22_solution.cpp
@@ -0,0 +1,28 @@
+////////// SOLUTION
+
+#include <vector>
+
+int solution(std::vector<int> &A) 
+{
+    int numberOnes = std::count(A.begin(), A.end(), 1), passingCars = 0;
+    
+    for (auto it = A.begin(); it != A.end(); it++)
+    {
+        if (*it == 0)
+        {
+            if (passingCars <= (1000000000 - numberOnes))
+                passingCars += numberOnes;
+            else 
+                return -1;
+        }
+        else
+            numberOnes--;
+    }
+    
+    return passingCars;
+}
+
+////////// CORRECT BEHAVIOUR
+////////// TIME COMPLEXITY:
+////////// MAX ~ 0(N)
+////////// AVG ~ 0(N)
