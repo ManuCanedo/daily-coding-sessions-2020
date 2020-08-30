7a04c5c59cecc77d671d932653075684a46eb0ec
Manu Canedo <chapi.gal@gmail.com>
2020-08-30 11:03:31 +0100

    Update and rename 08_30_AbsDistinct.cpp to AbsDistinct/08_30_solution.cpp

--- AbsDistinct/08_30_solution.cpp
+++ AbsDistinct/08_30_solution.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <set>
 
-int solution(vector<int> &A) 
+int solution(std::vector<int> &A) 
 {
     std::set<int> uniqueAbs;
     
-    for (int a : A)
+    for (int &a : A)
         uniqueAbs.insert(abs(a));
         
     return uniqueAbs.size();
