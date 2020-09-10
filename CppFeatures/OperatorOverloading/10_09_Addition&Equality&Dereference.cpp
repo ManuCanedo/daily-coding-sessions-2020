7920a745897df3c40967f7ba2c4755c084acb2e5
Manu Canedo <chapi.gal@gmail.com>
2020-09-10 14:47:37 +0100

    OperatorOverloading Examples

---
+++ CppFeatures/OperatorOverloading/10_09_Addition&Equality&Dereference.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+class Complex
+{
+public:
+    Complex(double re = 0.0, double im = 0.0)
+        : real(re), imaginary(im) {}
+
+    ~Complex() {}
+
+    Complex(const Complex &other)
+    {
+        *this = other;
+    }
+
+    const Complex &operator=(const Complex &other)
+    {
+        real = other.real;
+        imaginary = other.imaginary;
+        return *this;
+    }
+
+    bool operator==(const Complex &other) const
+    {
+        return real == other.real && imaginary == other.imaginary;
+    }
+
+    bool operator!=(const Complex &other) const
+    {
+        return !(*this == other);
+    }
+
+    Complex operator*() const
+    {
+        return Complex(real, -imaginary);
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Complex &other)
+    {
+        os << other.real << " " << other.imaginary << "i";
+        return os;
+    }
+
+    friend Complex operator+(const Complex &c1, const Complex &c2)
+    {
+        return Complex(c1.real + c2.real, c1.imaginary + c2.imaginary);
+    }
+
+    friend Complex operator+(const Complex &c1, const double c2)
+    {
+        return Complex(c1.real + c2, c1.imaginary);
+    }
+
+    friend Complex operator+(const double c1, const Complex &c2)
+    {
+        return Complex(c2.real + c1, c2.imaginary);
+    }
+
+private:
+    double real;
+    double imaginary;
+};
+
+int main()
+{
+    Complex c1(0.8, 0.2);
+    Complex c2(1.0, 0.5);
+
+    std::cout << c1 + c2 << std::endl;
+    std::cout << *c1 + *(c2 + 2.0) << std::endl;
+    
+    if (c1 == c2)
+        std::cout << "c1 and c2 are equal" << std::endl;
+    else
+        std::cout << "c1 and c2 are not equal" << std::endl;
+
+    return 0;
+}
---
+++ CppFeatures/OperatorOverloading/10_09_Assignment&LeftShift.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+class Test 
+{
+public:
+    // Test() 
+    //     : m_Id(0), m_Name("") {}
+
+    Test(int id = 0, std::string name = "") 
+        : m_Id(id), m_Name(name) {} 
+
+    ~Test() {}
+
+    Test(const Test &other)
+    {
+        *this = other;
+    }
+
+    const Test &operator=(const Test &other)
+    {
+        m_Id = other.m_Id;
+        m_Name = other.m_Name;
+        return *this;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Test &other)
+    {
+        os << other.m_Id << ": " << other.m_Name;
+        return os;
+    }
+
+private:
+    int m_Id;
+    std::string m_Name;
+};
+
+int main()
+{
+    Test test1;
+    Test test2(1, "Paco");
+
+    std::cout << test1 << " -- " << test2 << std::endl;
+
+    return 0;
+}
+
