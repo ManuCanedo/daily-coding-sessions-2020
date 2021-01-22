631b5704b85b241c260457e5a03dfa2d44365e8f
Manu Canedo <chapi.gal@gmail.com>
2021-01-22 14:25:37 +0000

    Added Test CUDA project

--- ParallelComputing_CUDA/CUDA_ArraySum/CUDA_ArraySum.cu
+++ ParallelComputing_CUDA/CUDA_ArraySum/CUDA_ArraySum.cu
@@ -1,3 +1,7 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <iostream>
 #include <memory>
 #include <math.h>
@@ -9,29 +13,29 @@ class Timer
 public:
     Timer() : m_StartTimepoint(std::chrono::high_resolution_clock::now()) {}
     ~Timer() { Stop(); }
-    
+
     void Stop()
     {
         const auto endTimepoint = std::chrono::high_resolution_clock::now();
         const auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
         const auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
         const double ms = (end - start) * 0.001;
-        std::cout << "Execution time: " << ms;
+        std::cout << "Execution time: " << ms << "ms\n";
     }
 
 private:
     const std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
 };
 
-// // CUDA add function
-// __global__
-// void addCUDA(int N, float *x, float *y)
-// {
-//     int index = threadIdx.x;
-//     int stride = blockDim.x;
-//     for (int i = index; i < N; i+=stride)
-//         y[i] += x[i];
-// }
+// CUDA add function
+__global__
+void addCUDA(int N, float* x, float* y)
+{
+    int index = threadIdx.x;
+    int stride = blockDim.x;
+    for (int i = index; i < N; i += stride)
+        y[i] += x[i];
+}
 
 // Standard CPU add function
 void addCPU(int N, float* x, float* y)
@@ -42,11 +46,11 @@ void addCPU(int N, float* x, float* y)
 
 int main()
 {
-    int N = 1<<20;
+    int N = 1 << 20;
 
     // Standard CPU array sum
-    float *x = new float[N], *y = new float[N];
-    
+    float* x = new float[N], * y = new float[N];
+
     for (int i = 0; i < N; ++i)
     {
         x[i] = 1.0f;
@@ -63,29 +67,29 @@ int main()
     delete[] y;
     x = y = nullptr;
 
-    // // CUDA array sum
-    // // Allocate Unified Memory
-    // cudaMallocManaged(&x, N * sizeof(float));
-    // cudaMallocManaged(&y, N * sizeof(float));
+    // CUDA array sum
+    // Allocate Unified Memory
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
 
-    // // Initialize x and y arrays
-    // for (int i = 0; i < N; ++i)
-    // {
-    //     x[i] = 1.0f;
-    //     y[i] = 2.0f;
-    // }
+    // Initialize x and y arrays
+    for (int i = 0; i < N; ++i)
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
 
-    // {
-    //     Timer CUDA;
-    //     // Run kernel on 1M elements on the CPU
-    //     add<<<1, 256>>>(N,x,y);
-    //     // Wait for GPU to finish before assessing the result
-    //     cudaDeviceSynchronize();
-    // }
+    {
+        Timer CUDA;
+        // Run kernel on 1M elements on the CPU
+        addCUDA << <1, N >> > (N, x, y);
+        // Wait for GPU to finish before assessing the result
+        cudaDeviceSynchronize();
+    }
 
-    // // Free Memory
-    // cudaFree(x);
-    // cudaFree(y);
+    // Free Memory
+    cudaFree(x);
+    cudaFree(y);
 
     return 0;
 }
